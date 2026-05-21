952c06f9c2e4c03784179856295f3a90d4f41241
Denys Shevchenko <den.sheva2000@gmail.com>
2026-05-21 22:33:55 +0300

    clean unused imports

--- src/main/java/ua/com/kneu/services/admins/AdminsService.java
+++ src/main/java/ua/com/kneu/services/admins/AdminsService.java
@@ -1,7 +1,6 @@
 package ua.com.kneu.services.admins;
 
 import ua.com.kneu.dtos.admins.AdminsDTO;
-import ua.com.kneu.entities.Admins;
 import ua.com.kneu.services.BaseService;
 
 public interface AdminsService extends BaseService<AdminsDTO> {
--- src/main/java/ua/com/kneu/services/clients/ClientsService.java
+++ src/main/java/ua/com/kneu/services/clients/ClientsService.java
@@ -1,7 +1,6 @@
 package ua.com.kneu.services.clients;
 
 import ua.com.kneu.dtos.clients.ClientsDTO;
-import ua.com.kneu.entities.Clients;
 import ua.com.kneu.services.BaseService;
 
 public interface ClientsService extends BaseService<ClientsDTO> {
