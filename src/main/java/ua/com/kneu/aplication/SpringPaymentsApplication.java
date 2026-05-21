8874752f991d8f08526189c651111f4f73817370
Denys Shevchenko <den.sheva2000@gmail.com>
2026-05-21 17:36:52 +0300

    config app

---
+++ src/main/java/ua/com/kneu/aplication/SpringPaymentsApplication.java
@@ -0,0 +1,11 @@
+package ua.com.kneu.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaymentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPaymentsApplication.class, args);
+    }
+}
